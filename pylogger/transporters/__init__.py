5177f3dcac44f40269bacab2a0e39a725b71c876
Gabriel S. Santiago <gabriel.santiago16@upr.edu>
2018-11-23 01:05:07 -0400

    Added DB transport.
    Currently only outputs logs  to one column in one table.

--- pylogger/transporters/__init__.py
+++ pylogger/transporters/__init__.py
@@ -1,5 +1,6 @@
 from .console import Console
 from .file import FileTransporter
 from .transporter import Transporter
+from .db import DbTransporter
 
-__all__ = ['Console', 'FileTransporter', 'Transporter']
+__all__ = ['Console', 'FileTransporter', 'Transporter', 'DbTransporter']
---
+++ pylogger/transporters/db.py
@@ -0,0 +1,45 @@
+from . import Transporter
+from .. import levels
+from ..protocols import Cursor, Connection
+import pylogger
+
+
+class DbTransporter(Transporter):
+    db_conn: Connection
+    insert_query = "INSERT INTO {table}({columns}) VALUES('{value}');"
+
+    def __init__(self, db_conn: Connection, table: str, column: str,
+                 level: levels.Levels=None, same_level: bool=True, trans_id: str=None):
+        super().__init__(level, same_level, trans_id)
+        self.table = table
+        self.column = column
+        self.db_conn = db_conn
+
+    # noinspection PyBroadException
+    def transport(self, message: object):
+        try:
+            cursor: Cursor = self.db_conn.cursor()
+
+            query = DbTransporter.insert_query.format(
+                columns=self.column,
+                table=self.table,
+                value=str(message).replace(";", '[semicolon]').replace('--', '[doubledash]').replace("'", '"')
+            )
+            cursor.execute(query)
+
+            self.db_conn.commit()
+            cursor.close()
+            cursor = None
+            del cursor
+        except Exception as e:
+            pylogger.PyLogger(
+                input_formats=[
+                    pylogger.formats.Timestamp,
+                    pylogger.formats.FileLine,
+                    pylogger.formats.FileCaller,
+                    pylogger.formats.ClassCaller
+                ],
+                transporters=pylogger.transporters.FileTransporter('txt/db_error.log'),
+                json=True,
+                level=pylogger.Levels.ERROR
+            ).error(str(e))
