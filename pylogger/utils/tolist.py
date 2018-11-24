45f94c591c94c3c119613038659b6398ed2767df
Gabriel S. Santiago <gabriel.santiago16@upr.edu>
2018-11-24 19:52:14 -0400

    Miscelaneous

--- pylogger/utils/tolist.py
+++ pylogger/utils/tolist.py
@@ -4,6 +4,9 @@ from typing import List
 def to_list(o: List[object] or object) -> object:
     ltr = list()
 
+    if o is None:
+        return ltr
+
     if isinstance(o, list):
         ltr.extend(o)
     else:
