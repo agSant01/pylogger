cabbe3bbf5155255e9aa20b2f60d313a61d12c61
Gabriel S. Santiago <gabriel.santiago16@upr.edu>
2018-11-24 19:51:40 -0400

    Added support for List of ColumnMeta in DbSchema

--- pylogger/protocols/db/schema.py
+++ pylogger/protocols/db/schema.py
@@ -4,14 +4,23 @@ from ...formats import Format
 
 
 class DbSchema:
-    def __init__(self, table: str, log_column: str, log_type_column: str, columns: Dict[str, Type[Format]]):
+    def __init__(self, table: str, log_column: str, log_type_column: str, columns: Dict[str, Type[Format]] or List[ColumnMeta]=None):
         self._table_name: str = table
         self.log_column: str = log_column
         self.log_type_column = log_type_column
 
         self._column_metadata: List[ColumnMeta] = list()
-        for name, fmt in columns.items():
-            self._column_metadata.append(ColumnMeta(name, fmt))
+
+        if columns is None:
+            return
+
+        if isinstance(columns, list):
+            if not isinstance(columns[0], ColumnMeta):
+                raise ValueError("Not a ColumnMeta object")
+            self._column_metadata.extend(columns)
+        else:
+            for name, fmt in columns.items():
+                self._column_metadata.append(ColumnMeta(name, fmt))
 
     def table(self) -> str:
         return self._table_name
